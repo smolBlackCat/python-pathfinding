d192bb7774f30e58e3c435b887c7dd854fe16e50
moura <smolBlackCat@proton.me>
2023-06-01 12:40:54 -0300

    add: Implement splash screen view

--- src/pathfinding/scenes.py
+++ src/pathfinding/scenes.py
@@ -3,23 +3,46 @@
 import sys
 from threading import Thread, active_count
 
-from basic_engine import scene
-from pygame import constants
+from basic_engine import scene, transition, interface
+from pygame import constants, image, time, transform
 
-from . import algorithms, cubes
+from . import algorithms, cubes, icon_path
 
 
 class SplashScreenScene(scene.Scene):
     """Scene that renders a splash screen."""
 
+    END_INTRO = constants.USEREVENT + 1
+
     def __init__(self, screen):
         super().__init__(screen)
 
-    def draw(self) -> None:
-        self.screen.fill((0, 0, 0))
+        self.bg_colour = (255, 255, 255)
+        self.app_icon = transform.scale(image.load(icon_path), (64, 64))
+        self.app_label = interface.Label(self.screen,
+                                         "App made by smolBlackCat",
+                                         colour=(0, 0, 0), size=36, chars_per_line=20)
+        self.app_icon_rect = self.app_icon.get_rect()
+        self.app_icon_rect.center = self.screen_rect.center
+        self.app_label.rect.center = self.screen_rect.center
+        self.app_label.rect.y += self.app_icon_rect.width + 30
 
-    def update(self) -> None:
-        return super().update()
+        time.set_timer(self.END_INTRO, 3000, 1)
+
+    def draw(self) -> None:
+        self.screen.fill(self.bg_colour)
+        self.screen.blit(self.app_icon, self.app_icon_rect)
+        self.app_label.draw()
+        
+
+    def update_on_event(self, event) -> None:
+        if event.type == self.END_INTRO:
+            self.scene_manager.change_scene(
+                "main",
+                transition.FadeTransition(
+                    self.screen, self.scene_manager, "main", self.bg_colour, 4
+                ),
+            )
 
 
 class ApplicationScene(scene.Scene):
